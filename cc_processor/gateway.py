e815e7a96dd73837e851d19ad777c0ccf6916956
Marija Ferber <marija.ferber@getbraintree.com>
2020-11-23 20:00:17 +0000

    Added refund customer to gateway

--- cc_processor/gateway.py
+++ cc_processor/gateway.py
@@ -30,3 +30,13 @@ class Gateway:
         customer.get_card(card_type).charge(charge_amount)
         return True
 
+    def merchant_refund_customer(self, merchant_name, customer_name, card_type, card_number, refund_amount, spending_limit):
+        merchant = self.get_merchant(merchant_name)
+        if merchant.get_customer(customer_name) == None:
+            self.merchant_add_customer(merchant_name, customer_name)
+        customer = merchant.get_customer(customer_name)
+        if customer.get_card(card_type) == None:
+            customer.add_card(card_type, card_number, spending_limit)
+        customer.get_card(card_type).refund(refund_amount)
+        return True
+
--- cc_processor/test_gateway.py
+++ cc_processor/test_gateway.py
@@ -37,3 +37,16 @@ def test_merchant_can_charge_customer(gateway):
     assert len(test_merchant.customers) == 1
     customer = test_merchant.get_customer(customer_name)
     assert customer.get_card(card_type).balance == 50.00
+
+def test_merchant_can_refund_customer(gateway):
+    merchant_name = "Planet Express"
+    customer_name = "Philip J Fry"
+    test_merchant = gateway.create_merchant(merchant_name)
+    card_type = "Platinum"
+    card_number = 4111111111111111
+    refund_amount = 25.00
+    spending_limit = 2500
+
+    assert gateway.merchant_refund_customer(merchant_name, customer_name, card_type, card_number, refund_amount, spending_limit) == True
+    customer = test_merchant.get_customer(customer_name)
+    assert customer.get_card(card_type).balance == -25.00
